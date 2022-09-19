35822ad41c9381a7018faa9b105e1d707fa9685e
MatthewMcMarvelous <terminatorlincoln@yahoo.com>
2022-09-19 09:22:14 -0500

    Added in craftable Tools.

---
+++ Resources/PrintTools.js
@@ -0,0 +1,27 @@
+async function GetToolsList() {
+    return fetch("../Resources/CraftableToolsList.json").then(res => res.json());
+}
+
+
+async function InsertToolsList(listId) {
+    const list = await GetToolsList();
+    let index = 0;
+    let listLength = list.length;
+
+    let domList = document.getElementById("CraftablesToolsListBody");
+
+    for(let i = 0; i < listLength; i++)
+    {
+        domList.innerHTML +=
+            `
+        <tr>
+            <td>` + list[i]["Name"] + `</td> 
+            <td>` + list[i]["Application"] + `</td> 
+            <td>` + list[i]["Ingredients"] + `</td> 
+            <td>` + list[i]["CraftCheck"] + `</td>
+            <td>` + list[i]["Value"] + `</td>
+        </tr>
+        `;
+    }
+}
+
